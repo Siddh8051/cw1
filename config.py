84c03aa587e174b371e5687994fdba0ddd2c7888
Siddh8051 <siddharthkumar805100@gmail.com>
2024-08-22 22:32:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 27815405
+api_id = 29029800
 
-api_hash = "4e70821cd2af3322f7cf2f2887e32821"
+api_hash = "0258b310e9ebb9b5155b5740d6ecf126"
 
-bot_token = "6360827839:AAFb5YQwkZSdJ9cWwuAjNisTkowsu2szcks"
+bot_token = "7374486550:AAHuv99oRb1VhU6sEgm6d_SV2Mdbrpb4zcw"
 
