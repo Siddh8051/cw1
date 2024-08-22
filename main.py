7392511b992e454296e9fb0c1c49c30d8b9fd89a
Siddh8051 <siddharthkumar805100@gmail.com>
2024-08-22 23:07:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,18 @@ import re
 import cloudscraper
 from bs4 import BeautifulSoup
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+    
+
 load_dotenv()
 os.makedirs("./downloads", exist_ok=True)
 
